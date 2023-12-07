9604833facd460e93dbc374258b200e192c5292c
yusuf <ybasori@gmail.com>
2023-12-08 00:37:37 +0700

    testing

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,18 +1,31 @@
 import express from "express";
+import { v4 } from "uuid";
 
-import ExpandRouter from "../Helpers/ExpandRouter";
-import Routes from "../Routes";
+// import ExpandRouter from "../Helpers/ExpandRouter";
+// import Routes from "../Routes";
 
 const app = express();
 
-ExpandRouter(Routes()).forEach((item) =>
-  item.controller
-    ? app[item.method](
-        item.path,
-        item.middleware ? item.middleware : [],
-        item.controller
-      )
-    : null
-);
+// ExpandRouter(Routes()).forEach((item) =>
+//   item.controller
+//     ? app[item.method](
+//         item.path,
+//         item.middleware ? item.middleware : [],
+//         item.controller
+//       )
+//     : null
+// );
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 export default app;
