75369054103aafe9ff2414f90e31c474d1b8983f
yusuf <ybasori@gmail.com>
2023-12-08 00:56:50 +0700

    testing

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const App_1 = __importDefault(require("./App"));
+const app = require("./App");
 const port = Number(process.env.PORT || 5000);
-App_1.default.listen(port, () => {
+app.listen(port, () => {
     // eslint-disable-next-line no-console
     console.log(`> ready on http://localhost:${port}`);
 });
