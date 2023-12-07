0e61db1eb7eaea6f80f6595daadf760f7dab095b
Yusuf <ybasori@gmail.com>
2023-12-07 23:54:06 +0700

    Create App.ts

---
+++ src/App.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import ExpandRouter from "./Helpers/ExpandRouter";
+import Routes from "./Routes";
+
+const port = Number(process.env.PORT || 5000);
+
+const app = express();
+
+ExpandRouter(Routes()).map((item) =>
+  item.controller
+    ? app[item.method](
+        item.path,
+        item.middleware ? item.middleware : [],
+        item.controller
+      )
+    : null
+);
+
+
+export default app
