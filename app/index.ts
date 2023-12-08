21695e38aef2c159041bf8e190e5fab00b4ca46a
yusuf <ybasori@gmail.com>
2023-12-08 11:33:59 +0700

    fix

--- app/index.ts
+++ app/index.ts
@@ -6,6 +6,7 @@ const port = Number(process.env.PORT || 5000);
 
 app.use(express.static("public"));
 app.listen(port, () => {
+  //
   // eslint-disable-next-line no-console
   console.log(`> ready on http://localhost:${port}`);
 });
