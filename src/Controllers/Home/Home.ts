25765b067a4ea6694ebd8986dd730ef744216560
yusuf <ybasori@gmail.com>
2023-12-08 08:35:46 +0700

    fix

--- src/Controllers/Home/Home.ts
+++ src/Controllers/Home/Home.ts
@@ -1,9 +1,6 @@
 import { RequestHandler } from "express";
 
 const Home: RequestHandler = (_req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-
   return res.status(200).json({
     status: "nice",
   });
