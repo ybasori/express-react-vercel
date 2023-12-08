d996947d9e475d2168c4dd52a5111e29d580c388
yusuf <ybasori@gmail.com>
2023-12-08 18:20:34 +0700

    oi

--- app/Controllers/Api/V1/PdfMerger/PdfMerger.ts
+++ app/Controllers/Api/V1/PdfMerger/PdfMerger.ts
@@ -1,36 +1,41 @@
-import { unlink } from "node:fs/promises";
+// import { unlink } from "node:fs/promises";
 
 import { RequestHandler } from "express";
-import PDFMerger from "pdf-merger-js";
+// import PDFMerger from "pdf-merger-js";
 
 const PdfMerger: RequestHandler = async (req, res) => {
-  try {
-    const mergedFile = `/uploads/merged-${Date.now()}.pdf`;
-    if (req.files) {
-      const totalFile = Number(req.files ? req.files.length : 0);
-      const files = [...(req.files as unknown as Express.Multer.File[])];
-      const merger = new PDFMerger();
-      for (let i: number = 0; i < totalFile; i++) {
-        await merger.add(files[i].path);
-      }
-      await merger.save(`./public${mergedFile}`);
+  // try {
+  //   const mergedFile = `/uploads/merged-${Date.now()}.pdf`;
+  //   if (req.files) {
+  //     const totalFile = Number(req.files ? req.files.length : 0);
+  //     const files = [...(req.files as unknown as Express.Multer.File[])];
+  //     const merger = new PDFMerger();
+  //     for (let i: number = 0; i < totalFile; i++) {
+  //       await merger.add(files[i].path);
+  //     }
+  //     await merger.save(`./public${mergedFile}`);
 
-      for (let i: number = 0; i < totalFile; i++) {
-        await unlink(files[i].path);
-      }
-    }
-    return res.status(200).json({
-      status: "oke",
-      data: {
-        file: mergedFile,
-      },
-    });
-  } catch (err) {
-    return res.status(400).json({
-      status: "error",
-      data: null,
-      error: err,
-    });
-  }
+  //     for (let i: number = 0; i < totalFile; i++) {
+  //       await unlink(files[i].path);
+  //     }
+  //   }
+  //   return res.status(200).json({
+  //     status: "oke",
+  //     data: {
+  //       file: mergedFile,
+  //     },
+  //   });
+  // } catch (err) {
+  //   return res.status(400).json({
+  //     status: "error",
+  //     data: null,
+  //     error: err,
+  //   });
+  // }
+
+  return res.status(200).json({
+    status: "oke",
+    data: null,
+  });
 };
 export default PdfMerger;
